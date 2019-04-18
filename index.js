73b8b8b920ddad22e04df08565aea122e94c96ba
KulaginArtur <artur.kulagin@gmail.com>
2019-04-18 09:54:14 +0300

    server commit

--- index.js
+++ index.js
@@ -6,9 +6,31 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// get the client
+const mysql = require('mysql2');
+
 app.get('/', (req, res) =>{
   res.send('Hello World');
 });
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+
+});
+
+app.get('/', (req, res) =>{
+// simple query
+connection.query(
+  'SELECT * FROM `table` ORDER BY name',
+  (err, results, fields) => {
+    console.log(results); // results contains rows returned by server
+    console.log(fields); // fields contains extra meta data about results, if available
+    res.send(result);
+  }
+);
 
 app.post('/',
     bodyParser.urlencoded({extended:true}),
