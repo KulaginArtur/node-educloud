a78c066b795110e140b422634875822b6921f8b2
KulaginArtur <artur.kulagin@gmail.com>
2019-04-18 09:49:55 +0300

    connecting to database

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 'use script';
 
 console.log('hello world!');
@@ -6,20 +7,42 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// get the client
+const mysql = require('mysql2');
+
 app.get('/', (req, res) =>{
   res.send('Hello World');
 });
 
-app.post('/',
-    bodyParser.urlencoded({extended:true}),
-    (req, res) =>{
-  console.log(req.body);
-  res.send('This is HTTP POST');
-});
-app.get('/test', (req, res) =>{
-  console.log(req.query);
-  res.send(`Hello ${req.query.name}!`)
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+
 });
 
+app.get('/', (req, res) =>{
+// simple query
+  connection.query(
+      'SELECT * FROM `table` ORDER BY name',
+      (err, results, fields) => {
+        console.log(results); // results contains rows returned by server
+        console.log(fields); // fields contains extra meta data about results, if available
+        res.send(result);
+      }
+  );
 
-app.listen(3000);
+  app.post('/',
+      bodyParser.urlencoded({extended:true}),
+      (req, res) =>{
+        console.log(req.body);
+        res.send('This is HTTP POST');
+      });
+  app.get('/test', (req, res) =>{
+    console.log(req.query);
+    res.send(`Hello ${req.query.name}!`)
+  });
+
+
+  app.listen(3000);
